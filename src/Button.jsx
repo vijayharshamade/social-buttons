d9ff49421f23881923aac09b82d027c2fec580c2
vijayharshamade <vharsha736.vh@gmail.com>
2024-03-17 01:59:26 +0530

    updated App.js

---
+++ src/Button.jsx
@@ -0,0 +1,10 @@
+//  import './Button.css'
+//  const Button = (props)=>{
+//     const {buttonDate} = props
+//     const {text,backgroundColor} = buttonDate
+//     return(
+//     <>
+//     <button className={`button ${backgroundColor}`}>{text}</button>
+//     </>
+// )}
+// export default Button
